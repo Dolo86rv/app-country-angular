60d8b7a90d46d8dcecbd26d47312891c57cdeb96
Dolo86rv <dolorv86@gmail.com>
2025-03-27 00:41:09 +0100

    Aplicando estilo a la pagina

--- src/app/country/mappers/country.mapper.ts
+++ src/app/country/mappers/country.mapper.ts
@@ -10,7 +10,9 @@ export class CountryMapper {
       flagSvg: item.flags.svg,
       name: item.translations['spa'].common ?? 'No spanish name',
       capital: item.capital.join(', '),
-      population: item.population
+      population: item.population,
+      region: item.region,
+      subregion: item.subregion
     }
   }
 
--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,12 +1,14 @@
-import { Component, inject, resource } from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
-import { of } from 'rxjs';
+
+import { NotFoundComponent } from "../../components/not-found/not-found.component";
+import { CountryInformationComponent } from "./country-information/country-information.component";
 
 @Component({
   selector: 'country-page',
-  imports: [],
+  imports: [NotFoundComponent, CountryInformationComponent],
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent {
